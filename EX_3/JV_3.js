4631480af1c945caa24540a32ee58d2af9bde009
Sam Thi Thuy <thuymieu44@gmail.com>
2026-05-23 12:32:04 +0900

    Add files via upload

---
+++ EX_3/JV_3.js
@@ -0,0 +1,33 @@
+let n= Number (prompt('Nhập vào 1 số từ 1 đến 9:'));
+switch(n){
+    case 0:
+        alert('Số Không');
+        break;
+    case 1:
+        alert('Số Một');
+        break;
+    case 2:
+        alert('Số Hai');
+        break;
+    case 3:
+        alert('Số Ba');
+        break;
+    case 4:
+        alert('Số Bốn');
+        break;
+    case 5:
+        alert('Só Năm');
+        break;
+    case 6:
+        alert('Số Sáu');
+        break;
+    case 7:
+        alert('Số Bảy');
+        break;
+    case 8:
+        alert('Số Tám');
+        break;
+    case 9:
+        alert('Số Chín');
+        break;
+}
